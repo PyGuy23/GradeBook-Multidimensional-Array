a35c86b14176f3d6d5b0230dec75977c743ba430
Timothy <eclisesolaris23@gmail.com>
2016-01-02 14:56:32 -0500

    Initial Commit

---
+++ GradeBook Multidimensional Array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GradeBook Multidimensional Array
+//
+//  Created by newuser on 1/2/16.
+//  Copyright © 2016 Gradebook Multidimenional Array. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
